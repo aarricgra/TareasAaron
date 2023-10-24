05bde436616369fc314b62dd25ed7349f423164d
aarricgra <aarricgra@alu.edu.gva.es>
2023-10-24 09:12:21 +0200

    1

--- app/src/main/java/com/example/tareasaaron/MainActivity.kt
+++ app/src/main/java/com/example/tareasaaron/MainActivity.kt
@@ -10,14 +10,22 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -26,11 +34,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,44 +56,105 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        Row (
-                            modifier = Modifier.fillMaxWidth().padding(top=16.dp).height(32.dp),
-                            horizontalArrangement = Arrangement.Center,
-                        ){
-                            Escribir()
-                            Button(
-                                modifier = Modifier.fillMaxHeight(),
-                                onClick = {
-                                   
-                                }
-                            ) {
-                                Text("Añadir")
-                            }
-                        }
-                    }
+                    MyApp()
                 }
             }
-
         }
     }
 
     @Composable
-    fun Escribir() {
+    fun MyApp() {
+        val taskList = remember { mutableListOf<Task>() }
         var textState by remember { mutableStateOf(TextFieldValue("")) }
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(top = 16.dp).height(32.dp),
+                horizontalArrangement = Arrangement.Center,
+            ) {
+                myTextField(textState) { newTextState ->
+                    textState = newTextState
+                }
+                Button(
+                    modifier = Modifier.fillMaxHeight(),
+                    shape = RectangleShape,
+                    onClick = {
+                        if(!textState.text.isEmpty()){
+                            taskList.add(Task(textState.text, false))
+                            textState = TextFieldValue("")
+                        }
+                    }
+                ) {
+                    Text("Añadir")
+                }
+            }
+
+            LazyColumn {
+                items(taskList) { task ->
+                    TaskItem(task = task, onTaskCheckedChange = { isChecked ->
+                        task.completed = isChecked
+                    }) {
+                        taskList.remove(task)
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun myTextField(
+        textState: TextFieldValue,
+        onValueChange: (TextFieldValue) -> Unit
+    ) {
         BasicTextField(
             value = textState,
-            onValueChange = { textState = it },
-            modifier= Modifier.
-                background(Color.White).
-                border(1.dp,Color.Black).
-                fillMaxHeight(),
-            textStyle= TextStyle(
-                color= Color.Black,
+            onValueChange = { newTextState ->
+                onValueChange(newTextState)
+            },
+            modifier = Modifier
+                .background(Color.White)
+                .border(1.dp, Color.Black)
+                .fillMaxHeight(),
+            textStyle = TextStyle(
+                color = Color.Black,
                 fontSize = 30.sp
             )
         )
+    }
+
+    data class Task(val name: String, var completed: Boolean)
+
+    @Composable
+    fun TaskItem(task: Task, onTaskCheckedChange: (Boolean) -> Unit, onDeleteClick: () -> Unit) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = task.completed,
+                onCheckedChange = { isChecked ->
+                    onTaskCheckedChange(isChecked)
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = task.name,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.weight(1f),
+            )
+            IconButton(
+                onClick = onDeleteClick
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Eliminar tarea",
+                )
+            }
+        }
     }
 }
